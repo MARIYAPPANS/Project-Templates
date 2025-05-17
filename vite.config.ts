b08bd22ca41546ef459fcefce878efc6631da701
Mariyappan <mariyappan.subramaniyan@zemosolabs.com>
2025-05-17 23:30:20 +0530

    File Modified Template

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@utils': path.resolve(__dirname, 'src/utils'),
-      '@components': path.resolve(__dirname, './src/components'),
-      '@assets': path.resolve(__dirname, './public/assets'),
-<<<<<<< HEAD
-=======
-      '@services':path.resolve(__dirname,'./services')
->>>>>>> 193e003c3554b5e63e4f64f199c6d0adf108366e
-    },
-  },
 })
